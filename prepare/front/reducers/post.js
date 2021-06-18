dffe84d72f25a23141159d8d452fed804840d662
mikyungyun <yun.workk@gmail.com>
2021-06-19 01:51:32 +0900

    reducer 용도에 맞게 분리해줌.

---
+++ prepare/front/reducers/post.js
@@ -0,0 +1,12 @@
+export const intialState = {
+  mainPosts: [],
+}
+
+const reducer = (state = intialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;

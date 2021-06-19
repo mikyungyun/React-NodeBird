d8c058066842890e3b6c77343705262a00c67a6d
mikyungyun <yun.workk@gmail.com>
2021-06-20 01:49:29 +0900

    오류 수정.

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,20 +1,22 @@
-import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, createStore, compose } from 'redux';
+import { createWrapper } from 'next-redux-wrapper';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from '../reducers';
 
 const configureStore = () => {
-  const middlewares = [] // 배열을 넣을 때 spread해서 넣어줌.
+  const middlewares = []; // 배열을 넣을 때 spread해서 넣어줌.
   const enhancer = process.env.NODE_ENV === 'production'
-    ? compose(applyMiddleware([...middlewares])) // 배포용
-    : composeWithDevTools(applyMiddleware([...middlewares])) // 개발용, dev툴을 넣어줌.
+    ? compose(applyMiddleware(...middlewares)) // 배포용
+    : composeWithDevTools(
+      applyMiddleware(...middlewares),
+    ); // 개발용, dev툴을 넣어줌.
   const store = createStore(reducer, enhancer);
   return store;
 };
 
 const wrapper = createWrapper(configureStore, {
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === 'development'
 });
 
 export default wrapper;

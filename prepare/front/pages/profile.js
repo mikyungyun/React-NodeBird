f98448c611f537c85d36d64f4982ec00f0a4b26b
mikyungyun <yun.workk@gmail.com>
2021-06-20 01:48:16 +0900

    팔로잉 팔로워 리스트 추가

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditionForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
-  return <AppLayout>내 프로필</AppLayout>
+  const followingList = [{ nickname: '미굥이' }, { nickname: '믹0' }, { nickname: '노드버드오피셜' }];
+  const followerList = [{ nickname: '미굥이' }, { nickname: '믹0' }, { nickname: '노드버드오피셜' }];
+
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <NicknameEditionForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
+    </>
+  )
 };
 
 export default Profile;

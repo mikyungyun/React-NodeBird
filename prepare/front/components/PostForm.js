4adb9710677df78bb7d08611dd19a1b9c12d90d1
mikyungyun <yun.workk@gmail.com>
2021-06-20 08:00:45 +0900

    이미지 파일 업로드.

--- prepare/front/components/PostForm.js
+++ prepare/front/components/PostForm.js
@@ -1,13 +1,22 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Form, Input, Button } from 'antd';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { addPost } from '../reducers/post';
 
 const PostForm = () => {
   const { imagePaths } = useSelector((state) => state.post);
-  const [text, onChangeText] = useState('');
-  const onSubmit = useCallback(() => {
-
+  const dispatch = useDispatch();
+  const imageInput = useRef(); //실제 dom에 접근하기 위해 사용.
+  const [text, setText] = useState('');
+  const onChangeText = useCallback((e) => {
+    setText(e.target.value);
   }, []);
+  const onSubmit = useCallback(() => {
+    dispatch(addPost); //dispatch 안에는 객체를 넣어주어야 함.
+    setText('');
+  }, []);
+  const onClickImageUpload = useCallback(() => { }, [imageInput.current]);
   return (
     <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
       <Input.TextArea
@@ -17,8 +26,8 @@ const PostForm = () => {
         placeholder="당신의 이야기를 들려주세요!"
       />
       <div>
-        <input type="file" multiple hidden />
-        <Button>이미지 업로드</Button>
+        <input type="file" multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
         <Button type="primary" style={{ float: 'right' }} htmlType="submit">짹짹</Button>
       </div>
       <div>
